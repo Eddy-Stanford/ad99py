015a3ed419e844f07dd035f727aa93499af63793
Robert King <robcking@stanford.edu>
2025-11-04 12:23:56 -0800

    format all

--- src/ad99py/_data.py
+++ src/ad99py/_data.py
@@ -4,32 +4,37 @@ import pathlib
 import os
 from importlib import resources
 from platformdirs import user_cache_dir
-from ._masks import get_xarray_mask,get_numpy_mask
+from ._masks import get_xarray_mask, get_numpy_mask
 from ._loon import process_flights
 
 PACKAGE_NAME = "ad99py"
-LOON_DATA_URL = 'https://stacks.stanford.edu/file/zh044ts5443/loon_GW_momentum_fluxes.csv'
-LOON_NC_MASK_NAME = 'loon_masks.nc'
-LOON_NP_MASK_NAME = 'loon_masks.npy'
-CACHE_ENV_VAR = 'AD99PY_CACHE_DIR'
+LOON_DATA_URL = (
+    "https://stacks.stanford.edu/file/zh044ts5443/loon_GW_momentum_fluxes.csv"
+)
+LOON_NC_MASK_NAME = "loon_masks.nc"
+LOON_NP_MASK_NAME = "loon_masks.npy"
+CACHE_ENV_VAR = "AD99PY_CACHE_DIR"
+
 
 def get_loon_nc_mask_path():
     cache_path = get_cache_dir() / LOON_NC_MASK_NAME
     if not cache_path.exists():
         mask_ds = get_xarray_mask()
         mask_ds.to_netcdf(cache_path)
-    return cache_path 
+    return cache_path
+
 
 def get_loon_np_mask_path():
     cache_path = get_cache_dir() / LOON_NP_MASK_NAME
     if not cache_path.exists():
         mask_np = get_numpy_mask()
-        np.save(cache_path,mask_np)
+        np.save(cache_path, mask_np)
     return cache_path
+
 
 def get_cache_dir() -> pathlib.Path:
     env_path = os.getenv(CACHE_ENV_VAR)
-    if env_path: 
+    if env_path:
         path = pathlib.Path(env_path).expanduser().resolve()
     else:
         path = pathlib.Path(user_cache_dir(PACKAGE_NAME))
@@ -43,42 +48,63 @@ def ensure_data(filename: str, url: str) -> pathlib.Path:
         print(f"Downloading {filename} to {cache_path}")
         r = requests.get(url)
         r.raise_for_status()
-        with open(cache_path, 'wb') as f:
+        with open(cache_path, "wb") as f:
             f.write(r.content)
     return cache_path
-    
+
+
 def download_loon_data():
     print(f"[INFO] Downloading Loon data from {LOON_DATA_URL}")
-    print("[INFO] Loon data is licensed under CC BY 4.0. Please cite the original source (Rhodes and Candido, 2021) when using this data.")
-    return ensure_data('loon_GW_momentum_fluxes.csv', LOON_DATA_URL)
+    print(
+        "[INFO] Loon data is licensed under CC BY 4.0. Please cite the original source (Rhodes and Candido, 2021) when using this data."
+    )
+    return ensure_data("loon_GW_momentum_fluxes.csv", LOON_DATA_URL)
+
 
 def save_loon_basins():
     loon_data = download_loon_data()
     masks = get_numpy_mask()
-    trop_atl_flights,extra_atl_flights,extra_pac_flights,indian_flights,trop_pac_flights,SO_flights = process_flights(loon_data,masks)
-    trop_atl_flights_path = get_cache_dir() / 'tropical_atlantic_flights_flux.npy'
-    extra_atl_flights_path = get_cache_dir() / 'extratropical_atlantic_flights_flux.npy'
-    extra_pac_flights_path = get_cache_dir() / 'extratropical_pacific_flights_flux.npy'
-    indian_flights_path = get_cache_dir() / 'indian_flights_flux.npy'
-    trop_pac_flights_path = get_cache_dir() / 'tropical_pacific_flights_flux.npy'
-    SO_flights_path = get_cache_dir() / 'southern_ocean_flights_flux.npy'
-    np.save(trop_atl_flights_path,trop_atl_flights) 
+    (
+        trop_atl_flights,
+        extra_atl_flights,
+        extra_pac_flights,
+        indian_flights,
+        trop_pac_flights,
+        SO_flights,
+    ) = process_flights(loon_data, masks)
+    trop_atl_flights_path = get_cache_dir() / "tropical_atlantic_flights_flux.npy"
+    extra_atl_flights_path = get_cache_dir() / "extratropical_atlantic_flights_flux.npy"
+    extra_pac_flights_path = get_cache_dir() / "extratropical_pacific_flights_flux.npy"
+    indian_flights_path = get_cache_dir() / "indian_flights_flux.npy"
+    trop_pac_flights_path = get_cache_dir() / "tropical_pacific_flights_flux.npy"
+    SO_flights_path = get_cache_dir() / "southern_ocean_flights_flux.npy"
+    np.save(trop_atl_flights_path, trop_atl_flights)
     print(f"[INFO] Saved Tropical Atlantic flights to {trop_atl_flights_path}")
-    np.save(extra_atl_flights_path,extra_atl_flights)
+    np.save(extra_atl_flights_path, extra_atl_flights)
     print(f"[INFO] Saved Extra Tropical Atlantic flights to {extra_atl_flights_path}")
-    np.save(extra_pac_flights_path,extra_pac_flights)
+    np.save(extra_pac_flights_path, extra_pac_flights)
     print(f"[INFO] Saved Extra Tropical Pacific flights to {extra_pac_flights_path}")
-    np.save(indian_flights_path,indian_flights)
+    np.save(indian_flights_path, indian_flights)
     print(f"[INFO] Saved Indian flights to {indian_flights_path}")
-    np.save(trop_pac_flights_path,trop_pac_flights)
+    np.save(trop_pac_flights_path, trop_pac_flights)
     print(f"[INFO] Saved Tropical Pacific flights to {trop_pac_flights_path}")
-    np.save(SO_flights_path,SO_flights)
+    np.save(SO_flights_path, SO_flights)
     print(f"[INFO] Saved Southern Ocean flights to {SO_flights_path}")
-    return (trop_atl_flights_path,extra_atl_flights_path,extra_pac_flights_path,indian_flights_path,trop_pac_flights_path,SO_flights_path)
+    return (
+        trop_atl_flights_path,
+        extra_atl_flights_path,
+        extra_pac_flights_path,
+        indian_flights_path,
+        trop_pac_flights_path,
+        SO_flights_path,
+    )
 
-def get_loon_basin_data(basin:str):
+
+def get_loon_basin_data(basin: str):
     path_name = get_cache_dir() / f"{basin}_flights_flux.npy"
     if not path_name.exists():
-        print(f"[INFO] Basin data for '{basin}' not found in cache. Generating basin data...")
+        print(
+            f"[INFO] Basin data for '{basin}' not found in cache. Generating basin data..."
+        )
         save_loon_basins()
     return path_name
--- src/ad99py/masks.py
+++ src/ad99py/masks.py
@@ -10,50 +10,70 @@ def list_basins(**kwargs):
     mask = load_mask(**kwargs)
     return [str(k) for k in mask.data_vars.keys()]
 
-def default_mask_path(dir=None,name=None)->str:
+
+def default_mask_path(dir=None, name=None) -> str:
     if dir is None or name is None:
         return get_loon_nc_mask_path()
-    return os.path.join(dir,name)
+    return os.path.join(dir, name)
 
 
-def load_mask(path:Optional[str]=None,recentering:bool=True,**kwargs)->xr.Dataset:
+def load_mask(
+    path: Optional[str] = None, recentering: bool = True, **kwargs
+) -> xr.Dataset:
     if path is None:
         path = default_mask_path(**kwargs)
     ds_mask = xr.open_dataset(path)
     if recentering:
         # map longitudes to be in range 0-360
-        ds_mask['lon']= (ds_mask.lon + 360)%360
+        ds_mask["lon"] = (ds_mask.lon + 360) % 360
     return ds_mask
 
 
-def mask_dataset(ds:xr.Dataset,basins=None,mask:Optional[xr.Dataset]=None,**kwargs):
+def mask_dataset(
+    ds: xr.Dataset, basins=None, mask: Optional[xr.Dataset] = None, **kwargs
+):
     if mask is None:
         mask = load_mask(**kwargs)
-    if 'latitude' in ds.variables and 'longitude' in ds.variables:
+    if "latitude" in ds.variables and "longitude" in ds.variables:
         # we assume either/or for 'latitude'/'longitude' or 'lat'/'lon
-        mask = mask.rename(lat='latitude',lon='longitude')
-        interp_mask = mask.interp(latitude=ds.latitude,longitude=ds.longitude,method='nearest')
+        mask = mask.rename(lat="latitude", lon="longitude")
+        interp_mask = mask.interp(
+            latitude=ds.latitude, longitude=ds.longitude, method="nearest"
+        )
         if basins is None:
             total_mask = sum(interp_mask[d] for d in interp_mask.data_vars)
         else:
             try:
                 total_mask = sum(interp_mask[d] for d in basins)
             except KeyError as e:
-                raise KeyError(f"No basin named {e.args[0]}. Allowed basins are {set(interp_mask.data_vars.keys())}") from e
-                
-        masked = ds.where(total_mask).stack(points=['latitude','longitude']).dropna('points',how='all')
-    elif 'lat' in ds.variables and 'lon' in ds.variables:
-        interp_mask = mask.interp(lat=ds.lat,lon=ds.lon,method='nearest')
+                raise KeyError(
+                    f"No basin named {e.args[0]}. Allowed basins are {set(interp_mask.data_vars.keys())}"
+                ) from e
+
+        masked = (
+            ds.where(total_mask)
+            .stack(points=["latitude", "longitude"])
+            .dropna("points", how="all")
+        )
+    elif "lat" in ds.variables and "lon" in ds.variables:
+        interp_mask = mask.interp(lat=ds.lat, lon=ds.lon, method="nearest")
         if basins is None:
             total_mask = sum(interp_mask[d] for d in interp_mask.data_vars)
         else:
             try:
                 total_mask = sum(interp_mask[d] for d in basins)
             except KeyError as e:
-                raise KeyError(f"No basin named {e.args[0]}. Allowed basins are {set(interp_mask.data_vars.keys())}") from e
-        masked = ds.where(total_mask).stack(points=['lat','lon']).dropna('points',how='all')
+                raise KeyError(
+                    f"No basin named {e.args[0]}. Allowed basins are {set(interp_mask.data_vars.keys())}"
+                ) from e
+        masked = (
+            ds.where(total_mask)
+            .stack(points=["lat", "lon"])
+            .dropna("points", how="all")
+        )
     else:
-        raise KeyError("Dataset must have 'latitude'/'longitude' or 'lat'/'lon' coordinates.")
-    masked = masked.reset_index('points')
-    return masked 
-   
+        raise KeyError(
+            "Dataset must have 'latitude'/'longitude' or 'lat'/'lon' coordinates."
+        )
+    masked = masked.reset_index("points")
+    return masked
